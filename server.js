e7eb1ddea7480c29ffe0858a8c150d8ecbb2a329
Katie Durga <katie.durga@gmail.com>
2020-02-12 11:32:23 -0500

    everything?

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "root",
-  database: "watch_listDB"
+  database: "employee_trackerDB"
 });
 
 connection.connect(function(err) {
@@ -30,6 +30,60 @@ connection.connect(function(err) {
   }
 
   console.log("connected as id " + connection.threadId);
+});
+
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM employees;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.render("index", { employees: data });
+  });
+});
+
+app.post("/api/employees", function(req, res) {
+  connection.query("INSERT INTO employees SET ?", [req.body.first_name, req.body.last_name, req.body.role_id, req.body.manager_id], function(err, result) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    // Send back the ID of the new employee
+    res.json({ id: result.insertId });
+    console.log({ id: result.insertId });
+  });
+});
+
+// Update a movie
+app.put("/api/employees/:id", function(req, res) {
+  connection.query("UPDATE employees SET first_name = ?, last_name = ?, role_id = ?, manager_id = ? WHERE id = ?", [req.body.first_name, req.body.last_name, req.body.role_id, req.body.manager_id, req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.changedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
+// Delete a movie
+app.delete("/api/employees/:id", function(req, res) {
+  connection.query("DELETE FROM employees WHERE id = ?", [req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
 });
 
 app.listen(PORT, function() {
